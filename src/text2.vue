9a84e52201c8ea96a00524a0f763a7df911f5450
wangtianwen <wangtianwen@kayak.com.cn>
2021-05-17 17:23:42 +0800

    updata

---
+++ src/text2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="box1">
+      <div class="one">*</div>
+      <div class="two">*</div>
+      <!-- <div class="three">*</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+.box1 {
+  height: 200px;
+  width: 200px;
+  background-color: aquamarine;
+  margin: auto;
+  display: flex;
+  /* justify-content: space-between; */
+  flex-direction: column;
+  /* align-items: center */
+}
+.one,.two,.three {
+  display: inline-block;
+  font-size: 50px
+}
+.two {
+  align-self: center;
+}
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
