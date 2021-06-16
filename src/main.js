b080dbd877cbdd9d9a0d3705d8179d32780063ce
wangtianwen <wangtianwen@kayak.com.cn>
2021-06-16 10:37:22 +0800

    [add]excel模块

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import text1 from './text1.vue'
-// import text2 from './text2.vue'
+import text2 from './text2.vue'
 import router from './router'
 import axios from 'axios'
 import store from './store'
